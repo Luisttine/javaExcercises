a1056e0b78071ad19c470628f95bb199ab450794
Luís Souza <63884418+Luisttine@users.noreply.github.com>
2022-03-21 19:23:24 -0300

    lista 2 monitoria

---
+++ Pacote_lista02/Teste.java
@@ -0,0 +1,11 @@
+package Pacote_lista02;
+
+import Pacote_lista02.Aluno;
+
+public class Teste {
+	public static void main(String[] args) {
+		Aluno aluno = new Aluno(123456789, "Maria", "Feminino", "551234567", "12345678920");
+
+		System.out.println(aluno);	
+	}
+}
